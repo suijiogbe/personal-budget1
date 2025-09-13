3052107e9f4b61a2b5598f3b2a7bc4948d408061
Stephanie Ijiogbe <sijiogbe@charlotte.edu>
2025-09-13 17:05:15 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 35
+        },
+        {
+            title: 'Rent',
+            budget: 375
+        },
+        {
+            title: 'Groceries',
+            budget: 110
+        },
+    ]
+};
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
